267ebd203d4a0c00cf692fe4134865da5f05d68b
Philipp Bönsch <philipp.boensch@softgames.de>
2020-12-21 00:26:51 +0100

    officer random wander

--- officer.gd
+++ officer.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 var chaising = false
+var target = null
 
 func _ready():
 	$"..".add_officer(self)
@@ -24,13 +25,17 @@ func chaise():
 	if $"bust_area".overlaps_area(player.get_node("area")):
 		$"..".loose()
 	
-	var speed = 40
+	var speed = 50
 	var dir = (player.position - position).normalized()
 	var vel = dir * speed
 	move_and_slide(vel)
 
 func idle():
-	pass
+	if target == null:
+		target = Vector2(randf() * 1024, 80 + randf() * 432)
+	move_and_slide((target - position).normalized() * 60)
+	if target.distance_to(position) < 10:
+		target = null
 
 func start_chaising():
 	print("start chaising")
