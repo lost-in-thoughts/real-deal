5ca673a9688f0b3e4c842976488d8df404fffc3a
ichinii <ogtoad@protonmail.com>
2020-12-21 01:05:39 +0100

    space in menu; officer stops chasing after a while

--- menu.gd
+++ menu.gd
@@ -5,18 +5,18 @@ func _ready():
 		if GameState.win:
 			if GameState.end():
 				$"win_or_loose".text = "everyone is high! thank you for playing"
-				$"click".text = "click to play again"
+				$"click".text = "press space to play again"
 			else:
 				$"win_or_loose".text = "you won! lets go to the neighborhood"
-				$"click".text = "click to play next level"
+				$"click".text = "press space to play next level"
 		else:
 			$"win_or_loose".text = "you lost! the officer has busted you"
-			$"click".text = "click to try again"
+			$"click".text = "press space to try again"
 
 func _process(delta):
 	pass
 
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == 1 and event.is_pressed():
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_SPACE:
 				get_tree().change_scene("res://level" + str(GameState.level) + ".tscn")
